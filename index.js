4a7c448a713cded2dce577f5a885ded971d31cf8
tomoko <mitchell.heaton@gmail.com>
2018-09-09 17:26:03 +1000

    Constants and beginning of readme

---
+++ index.js
@@ -0,0 +1,4 @@
+const cmdo = require('discord.js-commando');
+const client = new cmdo.Client();
+const tkn = require('./tokens.json');
+const ytdl = require('ytdl-core');
---
+++ readme.md
@@ -0,0 +1,6 @@
+# IzumiBot
+## Probably the most homosexual discord bot around
+
+## Requiremetns
+Discord.js-commando
+`npm install discord.js-commando`
