d7d6fcacbac4bd3fbf6e04f11b6790c490d65d50
tomoko <mitchell.heaton@gmail.com>
2018-09-09 18:14:06 +1000

    added a coinflip command

---
+++ commands/random/coinFlip.js
@@ -0,0 +1,23 @@
+const commando = require('discord.js-commando');
+
+class CoinFlipCommand extends commando.Command {
+  constructor(client) {
+    super(client, {
+      name: 'flip',
+      group: 'random',
+      memberName: 'flip',
+      description: 'flips a coin'
+    });
+  }
+
+  async run(message, args) {
+    var flip = Math.floor(Math.random() * 2)    // gets num, either 0 or 1
+    if (flip === 0) {
+        message.reply('it\'s Heads!')
+    } else {
+        message.reply('it\'s Tails!')
+    }
+  }
+}
+
+module.exports = CoinFlipCommand;
