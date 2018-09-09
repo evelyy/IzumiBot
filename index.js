1fffb84d26d560d4dc762f5d01252f6a13f767cd
tomoko <mitchell.heaton@gmail.com>
2018-09-09 18:02:41 +1000

    commented out youtube engine, make client start

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ const cmdo = require('discord.js-commando');
 const client = new cmdo.Client();
 const tkn = require('./tokens.json');
 const ytdl = require('ytdl-core');
-var youtube = require('./youtube.js');   // youtube engine
+// var youtube = require('./youtube.js');   // youtube engine
 
 var ytAudioQueue = [];
 var dispatcher = null;
 
-client.registry.registerCommandsIn('./commands/')
 client.registry.registerGroup('random', 'Random');
 client.registry.registerDefaults();
+client.registry.registerCommandsIn(__dirname + '/commands/')
+
+client.on('message', function(message) {
+    var messageParts = message.content.split(' ');
+    var command = messageParts[0].toLowerCase();
+    var parameters = messageParts.splice(1, messageParts.length);
+    console.log(`logged in as ${client.user.tag}!`)
+});
+
+client.login(tkn.d_token)
