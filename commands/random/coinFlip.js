7ff5c68856081065cbabd7ae662b1b07fb99868c
tomoko <mitchell.heaton@gmail.com>
2018-09-09 19:54:04 +1000

    cleaned run call

--- commands/random/coinFlip.js
+++ commands/random/coinFlip.js
@@ -10,7 +10,7 @@ class CoinFlipCommand extends commando.Command {
     });
   }
 
-  async run(message, args) {
+  async run(message) {
     var flip = Math.floor(Math.random() * 2)    // gets num, either 0 or 1
     if (flip === 0) {
         message.reply('it\'s Heads!')
